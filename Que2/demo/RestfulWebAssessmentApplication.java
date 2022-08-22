6591587f28f08c86d0ef2555eb6091f2afd1d77a
JayantUkey <110605408+JayantUkey@users.noreply.github.com>
2022-08-22 11:14:57 +0530

    Create RestfulWebAssessmentApplication.java

---
+++ Que2/demo/RestfulWebAssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebAssessmentApplication.class, args);
+		System.out.println("Assessment Que 2 Done");
+	}
+
+}
