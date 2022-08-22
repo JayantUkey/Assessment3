f48d6b2ee65d08eedcd2719421781da39b579107
JayantUkey <110605408+JayantUkey@users.noreply.github.com>
2022-08-22 11:16:45 +0530

    Create data.sql

---
+++ Que2/data.sql
@@ -0,0 +1,4 @@
+insert into customer values(1,'Jayant','9182736450','Nagpur','JayantUkey','jayAdmin');
+insert into customer values(2,'Nikhil','8475536450','Hydrabad','NikhilRaut','nikAdmin');
+insert into customer values(3,'Sarvesh','9752436450','Pune','SarveshDorve','sarAdmin');
+insert into customer values(4,'Vishal','7693456450','Banglore','VishalChore','visAdmin');
